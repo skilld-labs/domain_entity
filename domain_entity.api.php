a77113bf3f44aaec09909ca77af9b24d1bf0253d
delta <vincent.baronnet@gmail.com>
2013-05-26 05:13:52 +0200

    - Use DOMAIN_ACTIVE and DOMAIN_ALL constant. (when update from an older version, domain entity settings page must be visited, and the default values may need to be updated) - Not allowing not fieldable entity types. - Improve UI, messages, add collapsible fieldset. - Change static db_select by db_query when it's possible. - Add the possibility to programmatically alter the list of Domain access enabled entity types. add example in api.php

---
+++ domain_entity.api.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the domain_entity module.
+ */
+
+
+/**
+ * Allows modules to alter domain entity enabled entity, and their widget type by bundle.
+ *
+ * @param array $domain_entity_allowed_entity_types
+ *   The domain entity settings array.
+ */
+function hook_domain_entity_allowed_entity_types_alter(&$allowed_entity_types) {
+  if (isset($allowed_entity_types['commerce_order']) && strpos(current_path(), "all/my/commerce_order") === 0) {
+    // Disable domain access rules on commerce_order on the path all/my/commerce_order.
+    unset($allowed_entity_types['commerce_order']);
+  }
+}
