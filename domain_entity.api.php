a14a596100162dae70ec7f713623fb796a465eae
delta <vincent.baronnet@gmail.com>
2013-05-29 00:20:26 +0200

    add doc comments.

--- domain_entity.api.php
+++ domain_entity.api.php
@@ -10,7 +10,44 @@
  * Alter domain enabled entity types.
  *
  * Allows modules to alter domain entity enabled entity,
- * and their widget type by bundle.
+ * and their default assignation values by bundle.
+ *
+ * This array is structured as follow:
+ * array(
+ *   entity_type => array(
+ *     bundle => array(
+ *       widget_behaviour => array(
+ *         default_value => default_value,
+ *       ),
+ *     ),
+ *     other bundle => etc..
+ *   ),
+ *   other entity => etc..
+ * );
+ *
+ * Example :
+ * array(
+ *   'commerce_order' => array(
+ *     'commerce_order' => array(
+ *       DOMAIN_ENTITY_BEHAVIOR_USER => array(
+ *         DOMAIN_ACTIVE => DOMAIN_ACTIVE
+ *       ),
+ *     ),
+ *   ),
+ * );
+ *
+ * You can't change the widget by this hook, there where no effects.
+ *
+ * You can :
+ * You can unset an entity type from the array to disable query altering
+ * on this particular entity types.
+ *
+ * You can change the default assignation values of each entity types bundles.
+ *
+ * you can export configuration at install with that hook but you need to submit
+ * the domain configuration form after the installation. Except if you export
+ * the domain fields with feature or create it yourself after the install,
+ * see domain_entity_types_enable_domain_field($entity_types).
  *
  * @param array $allowed_entity_types
  *   The domain entity settings array.
